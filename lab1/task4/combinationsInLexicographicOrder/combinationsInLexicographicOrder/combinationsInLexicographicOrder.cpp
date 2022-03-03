2137714591f7826c6fdef735e358e44c94e4a1c3
d3nari <dungerway@gmail.com>
2022-03-03 15:45:25 +0300

    task 4 + semi completed

---
+++ lab1/task4/combinationsInLexicographicOrder/combinationsInLexicographicOrder/combinationsInLexicographicOrder.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <boost/timer.hpp>
+
+void PrintVector(std::vector<size_t>& c, int k) {
+	for (int i = 1; i < k + 1; ++i) {
+		std::cout << c[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+
+int main() {
+	boost::timer t;
+	t.restart();
+	int n = 5;
+	int k = 5;
+	std::vector<size_t> c(k+1);
+
+	c[0] = -1;
+	for (int i = 1; i < k; i++) { c[i] = i; }
+
+	int j = 1;
+	while (j != 0) {
+		PrintVector(c, k);
+		j = k;
+		while (c[j] == n - k + j) {
+			j = j - 1;
+		}
+		c[j] = c[j] + 1;
+		for (int i = j + 1; i < k; ++i) {
+			c[i] = c[i - 1] + 1;
+		}
+	} 
+
+
+	std::cout << std::endl;
+	double duration = t.elapsed();
+	std::cout << duration << std::endl;
+}
